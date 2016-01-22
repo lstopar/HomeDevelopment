ac44cc5e95c72e2c729111320a02cb09b2f6cbcd
Luka Stopar <luka.stopar@ijs.si>
2016-01-23 00:30:57 +0100

    ...

--- rpi/RpiNode/src/rpi.cpp
+++ rpi/RpiNode/src/rpi.cpp
@@ -305,9 +305,9 @@ void TYL40Adc::SetInput(const int& InputN) {
 	Notify->OnNotifyFmt(TNotifyType::ntInfo, "Reading YL-40 input %d ...", InputN);
 	EAssertR(0 <= InputN && InputN <= 3, "Invalid input channel: " + TInt::GetStr(InputN));
 //	uchar Command[2] = { uchar(InputN), 0x01u };
-	uchar Command[2] = { 0x40 | ((InputN + 1) & 0x03), uchar(TRnd().GetUniDevInt(256)) };
-	const int Written = write(FileDesc, &Command, 2);
-	EAssertR(Written == 2, "Failed to send a command to the YL-40 sensor: written " + TInt::GetStr(Written) +" bytes!");
+	uchar Command[2] = { uchar(0x40 | ((InputN + 1) & 0x03)), uchar(TRnd().GetUniDevInt(256)) };
+	write(FileDesc, &Command, 2);
+//	EAssertR(Written == 2, "Failed to send a command to the YL-40 sensor: written " + TInt::GetStr(Written) +" bytes!");
 
 //	SendCommand(Command);
 }
