fb6a0ec41a1e80c0dde890895c73e5d6a65a9c75
Luka Stopar <luka.stopar@ijs.si>
2016-01-29 01:30:22 +0100

    ...

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,20 +3,45 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo rpi-update
+# update RPi
+#sudo rpi-update
 
-sudo apt-get install uuid-dev
-sudo apt-get install i2c-tools
-sudo apt-get install libi2c-dev
+sudo apt-get install git tmux htop
+sudo apt-get install chkconfig
+sudo apt-get install uuid-dev i2c-tools libi2c-dev
+
+# install node 12
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash -
+sudo apt-get install --yes nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+# install node-gyp
+sudo npm install -g node-gyp
+
+# fix issue with rpcbind
+# follow instructions setup/rpcbind-instructions.txt
 
 # fix message "failed to execute /lib/udev/mtp-probe" at boot
 sudo apt-get install libmtp-runtime
+
 # remove the RPi network service as it is unreliable
 sudo apt-get remove raspberrypi-net-mods
 
-# server and torrents
+# setup I2C
+# open /boot/config.txt
+# uncomment line dtparam=i2c_arm=on
+
+# server
 sudo apt-get install apache2
 # enable the necessary modules
 sudo ln -s /etc/apache2/mods-available/proxy.load /etc/apache2/mods-enabled/proxy.load
 sudo ln -s /etc/apache2/mods-available/proxy_http.load /etc/apache2/mods-enabled/proxy_http.load
 sudo ln -s /etc/apache2/mods-available/proxy_wstunnel.load /etc/apache2/mods-enabled/proxy_wstunnel.load
+
+# torrents
+sudo apt-get install transmission-daemon
+
+# TODO must fix rpcbind to mount the storage
+
+# to mount the storage
+# sudo mount -t nfs -rw 192.168.1.100:/nfs/Public /mnt/nfs-wd
+
