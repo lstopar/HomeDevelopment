0bbb1649c806a493388dca7f1c7655171e682075
Luka Stopar <luka@lstopar.si>
2016-02-07 23:48:10 +0100

    ...

--- rpi/RpiNode/lib/librf24-bcm-V2/examples_RPi/pingpair_dyn.cpp
+++ rpi/RpiNode/lib/librf24-bcm-V2/examples_RPi/pingpair_dyn.cpp
@@ -1,238 +1,213 @@
 /*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- 
- 03/17/2013 : Charles-Henri Hallard (http://hallard.me)
-              Modified to use with Arduipi board http://hallard.me/arduipi
-						  Changed to use modified bcm2835 and RF24 library 
- */
+	TMRh20 2014 - Optimized RF24 Library Fork
+*/
 
 /**
- * Example RF Radio Ping Pair
+ * Example using Dynamic Payloads
  *
- * This is an example of how to use the RF24 class.  Write this sketch to two different nodes,
- * connect the role_pin to ground on one.  The ping node sends the current time to the pong node,
- * which responds by sending the value back.  The ping node can then see how long the whole cycle
- * took.
+ * This is an example of how to use payloads of a varying (dynamic) size.
  */
 
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include "./RF24.h"
 
+
+using namespace std;
 //
 // Hardware configuration
-//
+// Configure the appropriate pins for your connections
 
-// CE Pin, CSN Pin, SPI Speed
+/****************** Raspberry Pi ***********************/
 
-// Setup for GPIO 22 CE and GPIO 25 CSN with SPI Speed @ 1Mhz
-//RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_18, BCM2835_SPI_SPEED_1MHZ);
+// Radio CE Pin, CSN Pin, SPI Speed
+// See http://www.airspayce.com/mikem/bcm2835/group__constants.html#ga63c029bd6500167152db4e57736d0939 and the related enumerations for pin information.
 
 // Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 4Mhz
-//RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ); 
+//RF24 radio(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ);
 
-// Setup for GPIO 22 CE and CE1 CSN with SPI Speed @ 8Mhz
-//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_26, BCM2835_SPI_SPEED_8MHZ);
+// NEW: Setup for RPi B+
+//RF24 radio(RPI_BPLUS_GPIO_J8_15,RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ);
 
-const uint8_t CHANNEL = 0x4C;
+// Setup for GPIO 15 CE and CE0 CSN with SPI Speed @ 8Mhz
+//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 
-const uint8_t PinCE = RPI_V2_GPIO_P1_22;
-const uint8_t PinCSN = RPI_V2_GPIO_P1_24;
+/*** RPi Alternate ***/
+//Note: Specify SPI BUS 0 or 1 instead of CS pin number.
+// See http://tmrh20.github.io/RF24/RPi.html for more information on usage
 
-RF24 radio(PinCE, PinCSN, BCM2835_SPI_SPEED_8MHZ);
-//RF24 radio(22,0,BCM2835_SPI_SPEED_8MHZ);
+//RPi Alternate, with MRAA
+//RF24 radio(15,0);
+
+//RPi Alternate, with SPIDEV - Note: Edit RF24/arch/BBB/spi.cpp and  set 'this->device = "/dev/spidev0.0";;' or as listed in /dev
+//RF24 radio(22,0);
 
 
-// sets the role of this unit in hardware.  Connect to GND to be the 'pong' receiver
-// Leave open to be the 'ping' transmitter
-const int role_pin = 7;
+/****************** Linux (BBB,x86,etc) ***********************/
 
-//
-// Topology
-//
+// See http://tmrh20.github.io/RF24/pages.html for more information on usage
+// See http://iotdk.intel.com/docs/master/mraa/ for more information on MRAA
+// See https://www.kernel.org/doc/Documentation/spi/spidev for more information on SPIDEV
+
+// Setup for ARM(Linux) devices like BBB using spidev (default is "/dev/spidev1.0" )
+//RF24 radio(115,0);
+
+//BBB Alternate, with mraa
+// CE pin = (Header P9, Pin 13) = 59 = 13 + 46 
+//Note: Specify SPI BUS 0 or 1 instead of CS pin number. 
+//RF24 radio(59,0);
+
+/**************************************************************/
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
-//
-// Role management
-//
-// Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.  The hardware itself specifies
-// which node it is.
-//
-// This is done through the role_pin
-//
-
-// The various roles supported by this sketch
-typedef enum { role_ping_out = 1, role_pong_back } role_e;
-
-// The debug-friendly names of those roles
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};
-
-// The role of the current running sketch
-role_e role;
 
 
-int main(int argc, char** argv) {
-	//
-	// Role
-	//
+const int min_payload_size = 4;
+const int max_payload_size = 32;
+const int payload_size_increments_by = 1;
+int next_payload_size = min_payload_size;
 
-	// set up the role
-	role = role_ping_out;
+char receive_payload[max_payload_size+1]; // +1 to allow room for a terminating NULL char
 
-	//
-	// Print preamble:
-	//
+int main(int argc, char** argv){
 
-	printf("RF24/examples/pingtest/\n");
-	printf("ROLE: %s\n",role_friendly_name[role]);
-	printf("Starting ...\n");
+  bool role_ping_out = 1, role_pong_back = 0;
+  bool role = 0;
 
-	//
-	// Setup and configure rf radio
-	//
-	radio.begin();
+  // Print preamble:
+  cout << "RF24/examples/pingpair_dyn/\n";
 
-	// optionally, increase the delay between retries & # of retries
-	radio.setRetries(15,15);
+  // Setup and configure rf radio
+  radio.begin();
+  radio.enableDynamicPayloads();
+  radio.setRetries(15,15);
+  radio.setChannel(0x4c);
+  radio.printDetails();
 
-	// optionally, reduce the payload size.  seems to
-	// improve reliability
-	//  radio.setPayloadSize(8);
-	radio.setChannel(CHANNEL);
-	radio.setPALevel(RF24_PA_LOW);
 
-	//
-	// Open pipes to other nodes for communication
-	//
+/********* Role chooser ***********/
 
-	// This simple sketch opens two pipes for these two nodes to communicate
-	// back and forth.
-	// Open 'our' pipe for writing
-	// Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-	if ( role == role_ping_out )
-	{
-		radio.openWritingPipe(pipes[0]);
-		radio.openReadingPipe(1,pipes[1]);
+  printf("\n ************ Role Setup ***********\n");
+  string input = "";
+  char myChar = {0};
+  cout << "Choose a role: Enter 0 for receiver, 1 for transmitter (CTRL+C to exit) \n>";
+  getline(cin,input);
+
+  if(input.length() == 1) {
+	myChar = input[0];
+	if(myChar == '0'){
+		cout << "Role: Pong Back, awaiting transmission " << endl << endl;
+	}else{  cout << "Role: Ping Out, starting transmission " << endl << endl;
+		role = role_ping_out;
 	}
-	else
-	{
-		radio.openWritingPipe(pipes[1]);
-		radio.openReadingPipe(1,pipes[0]);
-	}
+  }
+/***********************************/
 
-	//
-	// Start listening
-	//
-	radio.startListening();
+    if ( role == role_ping_out )    {
+      radio.openWritingPipe(pipes[0]);
+      radio.openReadingPipe(1,pipes[1]);
+    } else {
+      radio.openWritingPipe(pipes[1]);
+      radio.openReadingPipe(1,pipes[0]);
+      radio.startListening();
+    }
 
-	//
-	// Dump the configuration of the rf unit for debugging
-	//
-	radio.printDetails();
-	//
-	// Ping out role.  Repeatedly send the current time
-	//
-	
-	// forever loop
+
+// forever loop
 	while (1)
 	{
-		if (role == role_ping_out)
-		{
-			// First, stop listening so we can talk.
-			radio.stopListening();
 
-			// Take the time, and send it.  This will block until complete
-			unsigned long time = millis();
-			printf("Now sending %lu...",time);
-			bool ok = radio.write( &time, sizeof(unsigned long) );
-			
-			if (ok)
-				printf("ok...");
-			else
-				printf("failed.\n");
+if (role == role_ping_out)
+  {
+    // The payload will always be the same, what will change is how much of it we send.
+    static char send_payload[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ789012";
 
-			// Now, continue listening
-			radio.startListening();
+    // First, stop listening so we can talk.
+    radio.stopListening();
 
-			// Wait here until we get a response, or timeout (250ms)
-			unsigned long started_waiting_at = millis();
-			bool timeout = false;
-			while ( ! radio.available() && ! timeout ) {
-					// by bcatalin » Thu Feb 14, 2013 11:26 am
-					delay(5); //add a small delay to let radio.available to check payload
-				if (millis() - started_waiting_at > 200 )
-					timeout = true;
-			}
+    // Take the time, and send it.  This will block until complete
+    printf("Now sending length %i...",next_payload_size);
+    radio.write( send_payload, next_payload_size );
 
+    // Now, continue listening
+    radio.startListening();
 
-			// Describe the results
-			if ( timeout )
-			{
-				printf("Failed, response timed out.\n");
-			}
-			else
-			{
-				// Grab the response, compare, and send to debugging spew
-				unsigned long got_time;
-				radio.read( &got_time, sizeof(unsigned long) );
+    // Wait here until we get a response, or timeout
+    unsigned long started_waiting_at = millis();
+    bool timeout = false;
+    while ( ! radio.available() && ! timeout )
+      if (millis() - started_waiting_at > 500 )
+        timeout = true;
 
-				// Spew it
-				printf("Got response %lu, round-trip delay: %lu\n",got_time,millis()-got_time);
-			}
+    // Describe the results
+    if ( timeout )
+    {
+      printf("Failed, response timed out.\n\r");
+    }
+    else
+    {
+      // Grab the response, compare, and send to debugging spew
+      uint8_t len = radio.getDynamicPayloadSize();
+      radio.read( receive_payload, len );
 
-			// Try again 1s later
-			//    delay(1000);
-			delay(1000);
-			
-		}
+      // Put a zero at the end for easy printing
+      receive_payload[len] = 0;
 
-		//
-		// Pong back role.  Receive each packet, dump it out, and send it back
-		//
+      // Spew it
+      printf("Got response size=%i value=%s\n\r",len,receive_payload);
+    }
 
-		if ( role == role_pong_back )
-		{
-			// if there is data ready
-			//printf("Check available...\n");
-			if ( radio.available() )
-			{
-				// Dump the payloads until we've gotten everything
-				unsigned long got_time;
-				bool done = false;
-				
-				while (!done)
-				{
-					// Fetch the payload, and see if this was the last one.
-					radio.read( &got_time, sizeof(unsigned long) );
+    // Update size for next time.
+    next_payload_size += payload_size_increments_by;
+    if ( next_payload_size > max_payload_size )
+      next_payload_size = min_payload_size;
 
-					// Spew it
-					printf("Got payload(%d) %lu...\n",sizeof(unsigned long), got_time);
+    // Try again 1s later
+    delay(100);
+  }
 
-					// Delay just a little bit to let the other unit
-					// make the transition to receiver
-					delay(20);
-				}
+  //
+  // Pong back role.  Receive each packet, dump it out, and send it back
+  //
 
-				// First, stop listening so we can talk
-				radio.stopListening();
+  if ( role == role_pong_back )
+  {
+    // if there is data ready
+    if ( radio.available() )
+    {
+      // Dump the payloads until we've gotten everything
+      uint8_t len;
 
-				// Send the final one back.
-				radio.write( &got_time, sizeof(unsigned long) );
+      while (radio.available())
+      {
+        // Fetch the payload, and see if this was the last one.
+	len = radio.getDynamicPayloadSize();
+	radio.read( receive_payload, len );
 
-				// Now, resume listening so we catch the next packets.
-				radio.startListening();
-			}
-		}
-	} // forever loop
+	// Put a zero at the end for easy printing
+	receive_payload[len] = 0;
 
-  return 0;
+	// Spew it
+	printf("Got payload size=%i value=%s\n\r",len,receive_payload);
+      }
+
+      // First, stop listening so we can talk
+      radio.stopListening();
+
+      // Send the final one back.
+      radio.write( receive_payload, len );
+      printf("Sent response.\n\r");
+
+      // Now, resume listening so we catch the next packets.
+      radio.startListening();
+    }
+  }
+}
 }
 
-// vim:cin:ai:sts=2 sw=2 ft=cpp
+
