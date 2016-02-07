4200f65601d8ce81199c68082abe6150027598a3
Luka Stopar <luka.stopar@ijs.si>
2016-02-07 20:24:27 +0100

    ...

--- rpi/RpiNode/lib/librf24/examples/pingtest.cpp
+++ rpi/RpiNode/lib/librf24/examples/pingtest.cpp
@@ -26,8 +26,8 @@
 
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 
-const uint8_t PinCE = RPI_V2_GPIO_P1_22;
-const uint8_t PinCSN = RPI_V2_GPIO_P1_24;
+//const uint8_t PinCE = RPI_V2_GPIO_P1_22;
+//const uint8_t PinCSN = RPI_V2_GPIO_P1_24;
 
 //RF24 radio(9,10);
 RF24 radio("/dev/spidev0.0",8000000 , 25);  //spi device, speed and CSN,only CSN is NEEDED in RPI
