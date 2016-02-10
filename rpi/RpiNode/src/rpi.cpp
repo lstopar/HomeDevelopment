3219a725b8e2cc18e403e8c793aa8ea063575795
Luka Stopar <luka.stopar@ijs.si>
2016-02-10 02:20:17 +0100

    ...

--- rpi/RpiNode/src/rpi.cpp
+++ rpi/RpiNode/src/rpi.cpp
@@ -377,7 +377,7 @@ void TRf24Radio::Init() {
 	Radio.setRetries(RETRY_DELAY, RETRY_COUNT);
 	Radio.setChannel(COMM_CHANNEL);
 	Radio.setDataRate(RF24_2MBPS);
-	Radio.setPALevel(RF24_PA_HIGH);
+	Radio.setPALevel(RF24_PA_LOW);
 	Radio.setPayloadSize(PAYLOAD_SIZE);
 	Radio.setCRCLength(RF24_CRC_8);
 	Radio.printDetails();
