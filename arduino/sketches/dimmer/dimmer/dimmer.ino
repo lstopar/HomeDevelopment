2f5d10870ec4d19cb830df6f6084c94c79e1abe1
Luka Stopar <luka.stopar@ijs.si>
2016-02-10 02:08:31 +0100

    ....

--- arduino/sketches/dimmer/dimmer/dimmer.ino
+++ arduino/sketches/dimmer/dimmer/dimmer.ino
@@ -1,19 +1,3 @@
-/*
-  // March 2014 - TMRh20 - Updated along with High Speed RF24 Library fork
-  // Parts derived from examples by J. Coliz <maniacbug@ymail.com>
-*/
-/**
- * Example for efficient call-response using ack-payloads 
- *
- * This example continues to make use of all the normal functionality of the radios including
- * the auto-ack and auto-retry features, but allows ack-payloads to be written optionally as well.
- * This allows very fast call-response communication, with the responding radio never having to 
- * switch out of Primary Receiver mode to send back a payload, but having the option to if wanting
- * to initiate communication instead of respond to a commmunication.
- */
- 
-
-
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -40,10 +24,7 @@ byte received[PAYLOAD_LEN];
 
 // Role management: Set up role.  This sketch uses the same software for all the nodes
 // in this system.  Doing so greatly simplifies testing.  
-
-typedef enum { role_ping_out = 1, role_pong_back } role_e;                 // The various roles supported by this sketch
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};  // The debug-friendly names of those roles
-role_e role = role_pong_back;                                              // The role of the current running sketch
+                                              // The role of the current running sketch
 
 // A single byte to keep track of the data being sent back and forth
 int currPin3Val = 0;
@@ -52,9 +33,6 @@ void setup(){
 
   Serial.begin(9600);
   printf_begin();
-  Serial.print(F("\n\rRF24/examples/pingpair_ack/\n\rROLE: "));
-  Serial.println(role_friendly_name[role]);
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
 
   Serial.println("Iniitalizing pins ...");
   pinMode(LED_PIN, OUTPUT);
