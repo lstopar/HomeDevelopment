5716ccf7fa06c40d240596e9fad5ffa698b719e5
Luka Stopar <luka.stopar@ijs.si>
2016-04-03 19:47:00 +0200

    ...

--- rpi/HomeServer/devices/devices.js
+++ rpi/HomeServer/devices/devices.js
@@ -368,7 +368,7 @@ module.exports = exports = function (_getValue, _setValue) {
 				nodes: [
 				    {
 				    	id: 'nodon-module',
-				    	internalId: 26243606,
+				    	internalId: 29206,
 				    	name: 'NodOn Relay',
 				    	type: 'D2-01-xx',
 				    	sensors: [
