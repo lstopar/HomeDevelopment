c1bed2a9811f3812d33ac0bdbb1cefcf4081e0c7
Luka Stopar <luka@lstopar.si>
2017-03-15 22:26:50 +0100

    fixed connected time visualization

--- rpi/HomeServer/src/utils.js
+++ rpi/HomeServer/src/utils.js
@@ -1,33 +1,36 @@
 var exec = require('child_process').exec;
 var config = require('../config.js');
 
+var HOURS_PER_DAY = 24;
+var DAYS_PER_MONTH = 30;
+
 module.exports = exports = {
 	getSensorImg: function (sensor) {
 		if (sensor.img != null) { return sensor.img; }
 		return 'img/missing.svg';
 	},
-		
+
 	formatVal: function (sensor, val) {
 		if (log.trace())
 			log.trace('Transforming value ' + val);
-		
+
 		return val.toFixed();
 	},
-	
+
 	formatConnTime: function (timestamp) {
 		var diff = new Date().getTime() - timestamp;
-		
+
 		var seconds = Math.floor(diff / 1000);
 		var minutes = Math.floor(seconds / 60);
 		var hours = Math.floor(minutes / 60);
-		var days = Math.floor(hours / 24);
-		var months = Math.floor(days / 30);
-		
+		var days = Math.floor(hours / HOURS_PER_DAY);
+		var months = Math.floor(days / DAYS_PER_MONTH);
+
 		if (months != 0) {
-			return months + 'M' + (days != 0 ? days + 'd' : '');
+			return months + 'M' + (days != 0 ? Math.floor(days % DAYS_PER_MONTH) + 'd' : '');
 		}
 		else if (days != 0) {
-			return days + 'd' + (hours != 0 ? hours + 'h' : '');
+			return days + 'd' + (hours != 0 ? Math.floor(hours % HOURS_PER_DAY) + 'h' : '');
 		}
 		else if (hours != 0) {
 			return hours + 'h';
@@ -39,7 +42,7 @@ module.exports = exports = {
 			return seconds + 's';
 		}
 	},
-	
+
 	ping: function (host, callback) {
 		exec('ping -c 2 -q ' + host, function (e, stdout, stderr) {
 			callback(e == null);
